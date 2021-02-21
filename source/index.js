0a264996e2715563f843cb45bc3a5f87c7aa064f
fedeghe <fedeghe@gmail.com>
2021-02-21 10:43:00 +0100

    try

--- source/index.js
+++ source/index.js
@@ -1,15 +1,15 @@
 import Monitor from './Monitor'
 import './style.less'
 
-window.Monitor = (function() {
-    var m = new Monitor();
-    var out = {
-        showFPS: options => {m.addPanel('fps', options); return out;},
-        showMEM: options => {m.addPanel('mem', options); return out;},
-        showTAGS: options => {m.addPanel('tags', options); return out;},
-        showEVENTS: options => {m.addPanel('events', options); return out;},
-        showNET: options => {m.addPanel('net', options); return out;},
-        render: () => m.render(document.body),
-    };
-    return out;
+(function() {
+    var m = new Monitor(),
+        out = {
+            showFPS: options => {m.addPanel('fps', options); return out;},
+            showMEM: options => {m.addPanel('mem', options); return out;},
+            showTAGS: options => {m.addPanel('tags', options); return out;},
+            showEVENTS: options => {m.addPanel('events', options); return out;},
+            showNET: options => {m.addPanel('net', options); return out;},
+            render: () => m.render(document.body),
+        };
+    window.Monitor = out;
 })();
