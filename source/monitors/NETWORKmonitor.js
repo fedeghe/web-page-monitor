e2bb46bbfe8e87d61a39864378e627e72df9c7f7
fedeghe <fedeghe@gmail.com>
2021-11-14 20:36:10 +0100

    what

--- source/monitors/NETWORKmonitor.js
+++ source/monitors/NETWORKmonitor.js
@@ -1,15 +1,22 @@
 import BaseMonitor from './BaseMonitor';
-import {extend, createNode, appendTo} from './utils';
+import {
+    extend,
+    createNode,
+    appendTo
+} from './utils';
 
-function NETWORKmonitor(options = {}){
+function NETWORKmonitor(options = {}) {
     this.$panel = createNode('div', {
         className: 'x-panel',
     });
-    var $title = createNode('div', {text: 'NETWORK', className: 'title'}),
+    var $title = createNode('div', {
+            text: 'NETWORK',
+            className: 'title'
+        }),
         $total = createNode('div'),
         factor = 2 ** 20;
     appendTo(this.$panel, [$title, $total]);
-    setInterval(function () {
+    setInterval(function() {
         var entries = performance.getEntries(),
             total = entries.reduce((acc, entry) => acc + (entry.transferSize || 0), 0);
         $total.innerHTML = `Total: ${(total / factor).toFixed(2)} MB`;
